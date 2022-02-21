a354f840b6ca531c6c9f5abf94055fad820b8ed1
Sumit K <synergy2411@outlook.com>
2022-02-21 11:43:30 +0530

    initial commit

---
+++ 00-basics/async.js
@@ -0,0 +1,91 @@
+// var flag = false;
+// function longRunningOp(){
+//     setTimeout(function(){
+//         console.log("Running the operation")
+//     }, 0)
+//     Promise.resolve("Promise API")
+//         .then(function(result){
+//             console.log(result)
+//             flag = true
+//         })
+// }
+
+// function webRequest(req){
+//     console.log("Start")
+//     longRunningOp()
+//     if(flag){
+//         console.log("End")
+//     }
+// }
+
+// webRequest({id : 1})
+
+// Start -> End -> Promise API -> Running the operation
+// Start -> Promise API -> Running the operation -> End
+
+
+
+
+// CALLBACK FUNCTION
+// function longRunningOp(cb){
+//     setTimeout(function(){
+//         console.log("Timer API");
+//         cb(afterEndProgram);
+//     }, 3000)
+// }
+
+// function afterEndProgram(){
+//     console.log("After program ends")
+// }
+
+// function endProgram(cb){
+//     console.log("End");
+//     cb();
+// }
+
+// function webRequest(){
+//     console.log("Start");
+//     longRunningOp(endProgram)   
+// }
+
+// webRequest()
+
+
+
+
+
+
+// PROMISE API
+
+function longRunningOp(){
+    var promise = new Promise(function(resolve, reject){
+        setTimeout(function(){
+            resolve({message : "SUCCESS"})
+        }, 3000)
+    })
+    return promise;
+}
+
+
+// USING ASYNC...AWAIT
+async function webRequest(){
+    console.log("START")
+    var result = await longRunningOp()
+    console.log(result);
+    console.log("END")
+}
+
+// USING THEN() STATEMENT
+// function webRequest(){
+//     console.log("Start")
+//     longRunningOp()
+//         .then(function(response){
+//             console.log(response);
+//             console.log("END")
+//         })
+//         .then(function(){
+//             console.log("After program ends")
+//         })
+// }
+
+webRequest();
---
+++ training_ref.md
@@ -0,0 +1,31 @@
+Break => 11:00 - 11:20 (20 Minutes)
+Lunch => 01:00 - 01:50 (50 Minutes)
+Break => 03:30 - 03:50 (20 Minutes)
+
+# What is JavaScript?
+- Front-end Code
+- Client-side Validation
+- Business Layer Code
+- Scripting Language (Embedded with HTML)
+- Event-Driven
+- Single-threaded
+- Asynchronous Code
+- Non-blocking
+- Interpreted (SpiderMonkey, Chakra, V8)
+- Light-weight
+- Fast & Performant App
+- Server-side Script
+- ECMA Community - ES2015 / ES6 (Next-Gen JavaScript)
+
+
+Full Stack App - MEAN | MERN | MEVN
+M - MongoDB (JSON format)
+E - Express (JS Framework)
+A/R/V - Angular / React / VueJS (Client-Side)
+N - NodeJS (Platform)
+
+
+Synchronous Task - Console.log, if/else, for
+Asynchronous Task - Timers, read/writing, socket, remote server call, DB Conn
+    - Macro : All Async task are Macro
+    - Micro : Promises, queueMicrotask
