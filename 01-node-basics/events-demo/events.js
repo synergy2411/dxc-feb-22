e2877cbd7a83412ab3e0ab47a48177ab14003fa8
Sumit K <synergy2411@outlook.com>
2022-02-22 17:18:52 +0530

    event demo added

---
+++ 01-node-basics/events-demo/events.js
@@ -0,0 +1,31 @@
+const EventEmitter = require("events").EventEmitter;
+const emitter = new EventEmitter();
+
+// on - register the subscriber / listener function
+// emit - emits/trigger/produce the event
+
+emitter.on("newListener", (eventName, listenerFn) => {
+  console.log(
+    eventName + " event added with Listener function " + listenerFn.name
+  );
+});
+emitter.on("removeListener", (eventName, listenerFn) => {
+  console.log("Unsubscribed");
+});
+const subscriberOne = (result) => {
+  console.log("First Subscription with message - ", result.message);
+};
+
+emitter.setMaxListeners(30)
+
+for (let i = 0; i < 25; i++) {
+  emitter.on("MyCustomEvent", subscriberOne);
+}
+
+emitter.on("MyCustomEvent", (result) => {
+  console.log("My Event Fired with message - ", result.message);
+  // emitter.removeListener("MyCustomEvent", subscriberOne);
+});
+
+emitter.emit("MyCustomEvent", { message: "SUCCESS" });
+emitter.emit("MyCustomEvent", { message: "WARNING" });
