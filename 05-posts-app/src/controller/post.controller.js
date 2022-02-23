3c5a24af5268a82c4ea8b1170bfb2ab69f5aefbe
Sumit K <synergy2411@outlook.com>
2022-02-23 17:06:02 +0530

    CRUD added

--- 05-posts-app/src/controller/post.controller.js
+++ 05-posts-app/src/controller/post.controller.js
@@ -20,6 +20,37 @@ const createPost = (req, res) => {
         })
 }
 
+const getSinglePost = async(req, res) => {
+    const { postId } = req.params;
+    try{
+        const result = await PostModel.findById(postId)
+        return res.send(result)
+    }catch(err){
+        console.log(err);
+        return res.send(err);
+    }
+}
+const deletePost = async(req, res) => {
+    const { postId } = req.params;
+    try{
+        const result = await PostModel.findByIdAndDelete(postId)
+        return res.send(result)
+    }catch(err){
+        console.log(err);
+        return res.send(err);
+    }
+}
+const updatePost = async(req, res) => {
+    const { postId } = req.params;
+    try{
+        const result = await PostModel.findByIdAndUpdate(postId, req.body)
+        return res.send(result)
+    }catch(err){
+        console.log(err);
+        return res.send(err);
+    }
+}
+
 module.exports = {
-    getAllPosts, createPost
+    getAllPosts, createPost, getSinglePost, deletePost, updatePost
 }
--- 05-posts-app/src/router/post.routes.js
+++ 05-posts-app/src/router/post.routes.js
@@ -1,15 +1,21 @@
 const express = require("express");
-const { getAllPosts, createPost } = require("../controller/post.controller");
+const {
+  getAllPosts,
+  createPost,
+  getSinglePost,
+  deletePost,
+  updatePost,
+} = require("../controller/post.controller");
 
 const PostRouter = express.Router();
 
-PostRouter.route("/")          // http://localhost:9090/posts
-    .get(getAllPosts)
-    .post(createPost)
+PostRouter.route("/") // http://localhost:9090/posts
+  .get(getAllPosts)
+  .post(createPost);
 
 PostRouter.route("/:postId")
-    .get()
-    .patch()
-    .delete()
+    .get(getSinglePost)
+    .patch(updatePost)
+    .delete(deletePost);
 
 module.exports = PostRouter;
