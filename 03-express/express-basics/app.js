8190bd9e14d9dcd0012b5583174a23ed88e9a781
Sumit K <synergy2411@outlook.com>
2022-02-23 12:45:26 +0530

    get single item

--- 03-express/express-basics/app.js
+++ 03-express/express-basics/app.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const app = express();
-const todoCollection = [];
+const todoCollection = [
+    {id : "1", label : "shopping", status : false},
+    {id : "2", label : "grocery", status : true},
+    {id : "3", label : "planting", status : false},
+];
 
 app.use(express.urlencoded({extended  : true}));            // Parse the request object and attach 'body' with request
 app.use(express.json());                         // Parse the json format and attach it with request body
+
+
+
+app.get("/api/todos/:todoId", (req, res) => {
+    const { todoId }  = req.params;
+    const foundTodo = todoCollection.find(todo => todo.id === todoId)
+    if(foundTodo){
+        return res.send(foundTodo)
+    }else{
+        return res.send({err : "NOT Found for ID - " + todoId}).status(404)
+    }
+})
 
 app.post("/api/todos", (req, res) => {
     console.log(req.body)
@@ -17,9 +33,8 @@ app.post("/api/todos", (req, res) => {
     res.send(todo)
 })
 
-app.get("/api/add-todo", (req, res)=>{
-    console.log(req.query);
-    res.send({message : "SUBMITTED"})
+app.get("/api/todos", (req, res)=>{
+    res.send(todoCollection);
 })
 
 
