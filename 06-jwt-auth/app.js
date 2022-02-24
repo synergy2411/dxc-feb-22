eb2a645c09a872c50ff0694e2dabec7cb10d6334
Sumit K <synergy2411@outlook.com>
2022-02-24 11:34:54 +0530

    token added

--- 06-jwt-auth/app.js
+++ 06-jwt-auth/app.js
@@ -1,16 +1,38 @@
 const express = require("express");
-
+const { sign, verify } = require("jsonwebtoken");
 const app = express();
+const SECRET_KEY = "MY_SUPER_SECRET_KEY";
 
 app.use(express.json());
 
 app.post("/user/login", (req, res) => {
-    console.log(req.body);
-    res.send({message : "SUCCESS"});
+    const { username, password } = req.body;
+    if(username && password){
+        const token = sign(req.body, SECRET_KEY, { expiresIn : "1d"})
+        return res.send({message : "SUCCESS", token});
+    }else{
+        return res.send({err : "Bad Credentials"})
+    }
 })
 
-app.get("/api/protected", (req, res) => {
-    res.send({message : "Protected API"})
+const ensureToken = (req, res, next) => {
+    const authHeader = req.headers["authorization"]
+    if(authHeader){
+        const token = authHeader.split(" ")[1]          // Bearer <token>
+        req.token = token;
+        next();
+    }
+    return res.send({err : "Auth header NOT found"})
+}
+
+app.get("/api/protected", ensureToken, (req, res) => {
+    verify(req.token, SECRET_KEY, (err, decode) => {
+        if(err){
+            return res.send(err)
+        }
+        console.log("DECODE : ", decode);
+        return res.send({message : "Authenticated User"})
+    })
 })
 
 app.listen(9091, () => console.log("Server started at PORT : 9091"));
