b0c72529240d363afba2f012fc4fa2428bd75afb
Sumit K <synergy2411@outlook.com>
2022-02-24 12:07:56 +0530

    db added

--- 06-jwt-auth/app.js
+++ 06-jwt-auth/app.js
@@ -1,38 +1,45 @@
 const express = require("express");
 const { sign, verify } = require("jsonwebtoken");
+require("./db");
+const UserModel = require("./model/user.model");
+
 const app = express();
 const SECRET_KEY = "MY_SUPER_SECRET_KEY";
 
 app.use(express.json());
 
-app.post("/user/login", (req, res) => {
-    const { username, password } = req.body;
-    if(username && password){
-        const token = sign(req.body, SECRET_KEY, { expiresIn : "1d"})
-        return res.send({message : "SUCCESS", token});
+app.post("/user/login", async (req, res) => {
+  try {
+    const foundUser = await UserModel.findOne(req.body);
+    if (foundUser) {
+      const token = sign({...foundUser._doc}, SECRET_KEY, { expiresIn: "1d" });
+      return res.send({ message: "SUCCESS", token });
     }else{
-        return res.send({err : "Bad Credentials"})
+        return res.send({ message: "User NOT found." });
     }
-})
+  } catch (err) {
+      return res.send({message : "Error", err})
+  }
+});
 
 const ensureToken = (req, res, next) => {
-    const authHeader = req.headers["authorization"]
-    if(authHeader){
-        const token = authHeader.split(" ")[1]          // Bearer <token>
-        req.token = token;
-        next();
-    }
-    return res.send({err : "Auth header NOT found"})
-}
+  const authHeader = req.headers["authorization"];
+  if (authHeader) {
+    const token = authHeader.split(" ")[1]; // Bearer <token>
+    req.token = token;
+    return next();
+  }
+  return res.send({ err: "Auth header NOT found" });
+};
 
 app.get("/api/protected", ensureToken, (req, res) => {
-    verify(req.token, SECRET_KEY, (err, decode) => {
-        if(err){
-            return res.send(err)
-        }
-        console.log("DECODE : ", decode);
-        return res.send({message : "Authenticated User"})
-    })
-})
+  verify(req.token, SECRET_KEY, (err, decode) => {
+    if (err) {
+      return res.send(err);
+    }
+    console.log("DECODE : ", decode);
+    return res.send({ message: "Authenticated User" });
+  });
+});
 
 app.listen(9091, () => console.log("Server started at PORT : 9091"));
